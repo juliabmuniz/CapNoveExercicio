016ce5971c39952d48873f500d0fac66786be9dd
Julia Muniz <juliabmuniz06@gmail.com>
2024-03-21 10:25:38 -0300

    Add project files.

---
+++ CapNoveExercicio/Entities/Order.cs
@@ -0,0 +1,89 @@
+﻿using CapNoveExercicio.Entities.Enum;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapNoveExercicio.Entities {
+    internal class Order {
+
+
+        public DateTime Moment { get; set; } 
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public Product Product { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order() {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client, Product product) {
+            Moment = moment;
+            Status = status;
+            Client = client;
+            Product = product;
+        }
+
+        public void AddItem(OrderItem item) {
+            Items.Add(item);
+
+        }
+
+        public void RemoveItem(OrderItem item) {
+            Items.Remove(item);
+
+        }
+        
+        public double Total() {
+            double valorFinal = 0.0;
+            foreach (OrderItem item in Items) {
+                valorFinal += item.SubTotal();
+
+            }
+
+            return valorFinal;
+        }
+
+        public override string ToString() {
+            StringBuilder sb = new StringBuilder();
+           
+            sb.AppendLine();
+            sb.AppendLine("ORDER SUMMARY: ");
+            sb.Append("Order moment: ");
+            sb.AppendLine(DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.Append("Order status: ");
+            sb.AppendLine(Status.ToString());
+            sb.Append("Client: ");
+            sb.Append(Client.Name);
+            sb.Append(" (");
+            sb.Append(Client.BirthDate.ToString("dd/MM/yyy"));
+            sb.Append(") - ");
+            sb.AppendLine(Client.Email);
+            sb.AppendLine("Order items: ");
+
+            foreach (OrderItem item in Items) {
+                sb.Append(item.Product.Name);
+                sb.Append(", $");
+                sb.Append(item.Product.Price);
+                sb.Append(", Quantity: ");
+                sb.Append(item.Quantity);
+                sb.Append(", Subtotal: ");
+                sb.AppendLine(item.SubTotal().ToString("F2", CultureInfo.InvariantCulture));
+            }
+            sb.Append("Total Price: $");
+            sb.AppendLine(Total().ToString("F2", CultureInfo.InvariantCulture));
+            
+            return sb.ToString();
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ CapNoveExercicio/Program.cs
@@ -0,0 +1,58 @@
+﻿using CapNoveExercicio.Entities.Enum;
+using System;
+using CapNoveExercicio.Entities;
+using System.Globalization;
+
+namespace CapNoveExercicio
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Enter client data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth Date: ");
+            DateTime birthdate = DateTime.Parse(Console.ReadLine());
+        
+            Client c1 = new Client(name, email, birthdate);
+
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status (PendingPayment/ Processing/ Shipped/ Delivered): ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Product p2 = new Product();
+            Order o = new Order(DateTime.Now, status, c1, p2);
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine("Enter #{0} item data: ", i);
+                Console.Write("Product name: ");
+                string productname = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+               
+                Product p1 = new Product(productname, price);
+                OrderItem o1 = new OrderItem(quantity, price, p1);
+                o.AddItem(o1);
+
+            }
+
+           
+           
+            Console.WriteLine(o);
+
+
+
+
+
+
+
+
+        }
+    }
+}
